88179deb019c71b64a0aba8f9845cf2e2e4277b7
Cory Grunkemeyer <cory.grunkemeyer@robotsandpencils.com>
2017-03-30 10:37:04 -0600

    Rough server working

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const dotenv = require('dotenv').config()
-const http = require('http')
+const express = require('express')
 const port = process.env.PORT || 5000 
 const leds = require('./src/leds');
+const request = require('request')
+const app = express()
 
-leds.blink(1,0,1)
+app.get('/', function (req, res) {
+  res.send('Hi there.')
+})
 
-function handleRequest(request, response){
-  response.end('It Works!! Path Hit: ' + request.url)
-}
+app.post('/post', function (req, res) {
+  console.log(req)
+  res.send('POST request recieved.')
+  leds.blink(1,0,1)
+  res.end()
+})
 
-var server = http.createServer(handleRequest)
-
-server.listen(port, function(){
-  console.log("Server listening on: http://localhost:%s", port)
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
 })
